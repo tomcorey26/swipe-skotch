b95353e2b4f5630bcdb3881f3fd7990f0890903a
Tom Corey <tomcorey26@my.uri.edu>
2020-05-17 19:44:59 -0400

    react

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,10 +1,12 @@
 import express from 'express';
 
 const app = express();
-const port = 3000;
+
 app.get('/', (req, res) => {
   res.send('The sedulous hyena ate the antelope!');
 });
+
+const port = 3000;
 app.listen(port, (err) => {
   if (err) {
     return console.error(err);
